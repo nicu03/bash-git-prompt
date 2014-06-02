4b7fd29bef41e65dadff0e46791146ee544a9300
nicu03 <nicolae.natrapeiu@gmail.com>
2014-06-02 23:37:58 +0300

    added a trailing space just to fix an overlalpping for some fonts (like fantasque-mono)

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -7,14 +7,14 @@ GIT_PROMPT_SUFFIX="]"                 # the end of the git info string
 GIT_PROMPT_SEPARATOR="|"              # separates each item
 
 GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
-GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
-GIT_PROMPT_CONFLICTS="${Red}✖"        # the number of files in conflict
-GIT_PROMPT_CHANGED="${Blue}✚"         # the number of changed files
+GIT_PROMPT_STAGED="${Red}● "           # the number of staged files/directories
+GIT_PROMPT_CONFLICTS="${Red}✖ "        # the number of files in conflict
+GIT_PROMPT_CHANGED="${Blue}✚ "         # the number of changed files
 
 GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
-GIT_PROMPT_UNTRACKED="${Cyan}…"       # the number of untracked files/dirs
-GIT_PROMPT_STASHED="${BoldBlue}⚑"     # the number of stashed files/dir
-GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" repo
+GIT_PROMPT_UNTRACKED="${Cyan}… "       # the number of untracked files/dirs
+GIT_PROMPT_STASHED="${BoldBlue}⚑ "     # the number of stashed files/dir
+GIT_PROMPT_CLEAN="${BoldGreen}✔ "      # a colored flag indicating a "clean" repo
 
 GIT_PROMPT_START_USER="\n╔═╡${Yellow}${PathShort}${ResetColor}"
 GIT_PROMPT_START_ROOT="${Red}╔═╡${Yellow}${PathShort}${ResetColor}"
